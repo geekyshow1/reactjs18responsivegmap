76d7ef742ffe2b726f9ef5501f9cf9f382118275
Rajesh <geekyshows@gmail.com>
2023-12-12 10:02:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <address id='address'>
+        Bokaro Steel City College <br />
+        Sec-6, Bokaro, Jharkhand - 827006
+      </address>
+      <div className='responsive-map'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.689695677483!2d86.1738957752323!3d23.687052478713632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4218a078e5d3d%3A0xe30f0dd8e378ecde!2sBokaro%20Steel%20City%20College!5e0!3m2!1sen!2sin!4v1702355237861!5m2!1sen!2sin" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
